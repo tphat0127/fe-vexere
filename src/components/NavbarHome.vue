7f79effbf1a56f38ebbc96b221fafb59f687c844
Tan Phat <tphat0127@gmail.com>
2020-11-14 05:05:34 +0700

    Add ant design

---
+++ src/components/NavbarHome.vue
@@ -0,0 +1,36 @@
+<template>
+    <a-row type="flex" justify="center" class="header">
+      <div class="logo" />
+      <a-menu
+        theme="light"
+        mode="horizontal"
+        :default-selected-keys="['1']"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          Trang chủ
+        </a-menu-item>
+        <a-menu-item key="2">
+          Station
+        </a-menu-item>
+        <a-menu-item key="3">
+          Liên hệ
+        </a-menu-item>
+        <a-menu-item key="4">
+          <UserOutlined />Đăng nhập
+        </a-menu-item>
+      </a-menu>
+    </a-row>
+</template>
+<script>
+import {UserOutlined} from "@ant-design/icons-vue";
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    UserOutlined,
+  }
+};
+</script>
---
+++ src/views/HomeTemplate/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavbarHome />
+  <router-view />
+</template>
+
+<script>
+import NavbarHome from "./../../components/NavbarHome";
+export default {
+  components: {
+    NavbarHome,
+  },
+};
+</script>
+
+<style></style>
