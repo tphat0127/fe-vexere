c189d7ad11e2da3fffe99ec520efd42e62c9ece4
tphat0127 <tphat0127@gmail.com>
2020-11-17 17:22:08 +0700

    login auth design

--- src/views/AdminTemplate/AuthPage/index.vue
+++ src/views/AdminTemplate/AuthPage/index.vue
@@ -1,42 +1,33 @@
 <template>
-  <a-row type="flex" justify="center" align="middle">
-  <a-col v-if="loading"><Loader /></a-col>
-  <a-col v-else>
-    <h2 align="center">Login</h2>
+  <div class="form">
+    <div class="logo"><img alt="logo" src="/favicon.ico"><span>Login Admin</span></div>
     <a-alert v-if="error" type="error" :message="error.response.data.message" banner />
-    <a-form layout="horizontal" :model="formInline" @submit.prevent="handleSubmit">
-        <a-form-item>
-            <a-input v-model:value="formInline.email" placeholder="Username">
-            <template #prefix><UserOutlined style="color:rgba(0,0,0,.25)"/></template>
-            </a-input>
-        </a-form-item>
-        <a-form-item>
-            <a-input v-model:value="formInline.password" type="password" placeholder="Password">
-            <template #prefix><LockOutlined style="color:rgba(0,0,0,.25)"/></template>
-            </a-input>
-        </a-form-item>
-        <a-form-item>
-            <a-button
-            type="primary"
-            html-type="submit"
-            :disabled="formInline.user === '' || formInline.password === ''"
-            >
-            Log in
-            </a-button>
-        </a-form-item>
-      </a-form>
-  </a-col>
-  </a-row>
+    <a-form layout="vertical" @submit.prevent="handleSubmit">
+      <a-form-item>
+          <a-input v-model:value="formInline.email" placeholder="Username" />
+      </a-form-item>
+      <a-form-item>
+          <a-input v-model:value="formInline.password" type="password" placeholder="Password" />
+      </a-form-item>
+      <a-form-item>
+          <a-button
+          type="primary"
+          html-type="submit"
+          :disabled="formInline.user === '' || formInline.password === ''"
+          :loading="loading"
+          >
+          Log in
+          </a-button>
+      </a-form-item>
+    </a-form>
+    <router-link to="/"> ← Back to Home Page </router-link>
+  </div>
 </template>
+
 <script>
-import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
-import Loader from "./../../../components/Loader";
 import * as types from "./../../../store/auth/constant";
 export default {
   components: {
-    UserOutlined,
-    LockOutlined,
-    Loader
   },
   data() {
     return {
@@ -65,3 +56,45 @@ export default {
   }
 };
 </script>
+
+<style>
+  body {
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  .form{
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    margin: -160px 0 0 -160px;
+    width: 320px;
+    height: 320px;
+    padding: 36px;
+    box-shadow: 0 0 100px rgba(0,0,0,.08);
+  }
+  .logo{
+    text-align: center;
+    cursor: pointer;
+    margin-bottom: 24px;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -ms-flex-align: center;
+    align-items: center;
+  }
+  .logo span {
+    vertical-align: text-bottom;
+    font-size: 16px;
+    text-transform: uppercase;
+    display: inline-block;
+    font-weight: 700;
+    color: #1890ff;
+    background-image: -webkit-gradient(linear,37.219838% 34.532506%,36.425669% 93.178216%,from(#29cdff),to(#0a60ff),color-stop(.37,#148eff));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+</style>
---
+++ src/views/AdminTemplate/DashboardPage/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>dcs</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
