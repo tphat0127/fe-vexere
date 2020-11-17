efe604f32c5595182f5615d3e3f146b357682bc1
tphat0127 <tphat0127@gmail.com>
2020-11-17 17:52:35 +0700

    style admin template

---
+++ src/components/MenuAdmin/index.vue
@@ -0,0 +1,29 @@
+<template>
+<a-menu theme="light" v-model:selectedKeys="selectedKeys" mode="inline">
+    <a-menu-item key="1">
+        <router-link to="/admin/stations">
+        <PieChartOutlined/> <span>Station </span></router-link>
+    </a-menu-item>
+    <a-menu-item key="2">
+        <router-link to="/about">
+        <FileOutlined />Liên hệ</router-link>
+    </a-menu-item>
+</a-menu>
+</template>
+
+<script>
+import {
+  PieChartOutlined,
+  FileOutlined,
+} from '@ant-design/icons-vue';
+export default {
+    components: {
+        PieChartOutlined,
+        FileOutlined
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/AdminTemplate/index.vue
+++ src/views/AdminTemplate/index.vue
@@ -1,16 +1,46 @@
 <template>
-  <NavbarAdmin />
-  <div style="margin-bottom: 2em"></div>
-  <router-view />
+  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible theme="light">
+      <div class="logo" />
+      <MenuAdmin />
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content style="margin: 0 16px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Admin</a-breadcrumb-item>
+          <a-breadcrumb-item>...</a-breadcrumb-item>
+        </a-breadcrumb>
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+          <router-view />
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script>
-import NavbarAdmin from "./../../components/NavbarAdmin";
+
+import MenuAdmin from "./../../components/MenuAdmin"
 export default {
   components: {
-    NavbarAdmin
-  }
+    MenuAdmin
+  },
+  data() {
+    return {
+      collapsed: false,
+      selectedKeys: ['1'],
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+#components-layout-demo-side .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
