8320aec3df3c36918c92452612371337ff4b1618
tphat0127m <tphat0127@gmail.com>
2021-01-04 02:54:53 +0700

    change api url

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   //https://ntp-cybersoft-be.herokuapp.com
-  baseURL: "http://localhost:5555/api"
+  baseURL: "https://ntp-cybersoft-be.herokuapp.com/api"
 });
 export const imgServer = "https://ntp-cybersoft-be.herokuapp.com/images/"
